8cc6950790ef11caff82200d33625b8f4a9ae170
yuvalbenshitrit <yuval056@gmail.com>
2025-07-02 15:39:22 +0300

    create tests

---
+++ test_main_page.py
@@ -0,0 +1,10 @@
+from selenium.webdriver.common.by import By
+import time
+
+def test_main_page(driver):
+    """Test the main page loads correctly."""
+    time.sleep(2)
+    print("main page loaded successfully.")
+
+
+
